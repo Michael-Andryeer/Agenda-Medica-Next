c7dedbbff583bf5a74e731a36da560ca0444fb8e
michael-andryeer <andryeerm@gmail.com>
2025-05-30 16:27:15 -0300

    feat: Gerenciamento de médicos e agendamentos

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,3 +1,7 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import {
   PageActions,
   PageContainer,
@@ -7,24 +11,43 @@ import {
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
-import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
+import { db } from "@/db";
+import { doctorsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
 
-const DoctorsPage = () => {
+import AddDoctorButton from "./_components/add-doctor-button";
+import DoctorCard from "./_components/doctor-card";
+
+const DoctorsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+  const doctors = await db.query.doctorsTable.findMany({
+    where: eq(doctorsTable.clinicId, session.user.clinic.id),
+  });
   return (
     <PageContainer>
-      <PageHeaderContent>
-        <PageTitle>Médicos</PageTitle>
-        <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
-      </PageHeaderContent>
-      <PageActions>
-        <Button>
-          <PlusIcon className="" />
-          Novo médico
-        </Button>
-      </PageActions>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <AddDoctorButton />
+        </PageActions>
+      </PageHeader>
       <PageContent>
-        <h1>Médicos</h1>
+        <div className="grid grid-cols-3 gap-6">
+          {doctors.map((doctor) => (
+            <DoctorCard key={doctor.id} doctor={doctor} />
+          ))}
+        </div>
       </PageContent>
     </PageContainer>
   );
